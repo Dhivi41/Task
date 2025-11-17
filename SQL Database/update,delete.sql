79179a022f38b83420ab694570ceeb310ac3e37c
Dhivi41 <dhiviraj2004@gmail.com>
2025-11-17 21:43:45 +0530

    50.rename,update,delete

--- SQL Database/update,delete.sql
+++ SQL Database/update,delete.sql
@@ -4,11 +4,14 @@ ALTER TABLE products
 ADD COLUMN stock INT;
 ALTER TABLE products 
 CHANGE COLUMN price unit_price DECIMAL(10,2);
+DESC products;
 INSERT INTO products (id, name, unit_price, stock) 
 VALUES (1, 'Laptop', 55000.00, 10),
 (2, 'Keyboard', 1200.00, 50),
 (3, 'Mouse', 600.00, 40),
 (4, 'Monitor', 8000.00, 15),
 (5, 'Headphones', 1500.00, 25);
+SELECT * FROM products;
 UPDATE products SET stock = 60 WHERE id = 3;
 DELETE FROM products WHERE id = 5;
+SELECT * FROM products;
