4d3838d2082a306c2f8683bb343397c741ed8a77
Dhivi41 <dhiviraj2004@gmail.com>
2026-01-08 18:06:20 +0530

    10. Day 9 Introduction to Arrays

---
+++ Java/array/ReplaceNegative.java
@@ -0,0 +1,25 @@
+package array;
+import java.util.Scanner;
+public class ReplaceNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        
+        int n = sc.nextInt();
+       int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                arr[i] = 0;
+            }
+        }
+        System.out.println("Updated array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
