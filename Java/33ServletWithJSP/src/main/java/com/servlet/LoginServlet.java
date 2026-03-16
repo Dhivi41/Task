0544bc37102569104dea066686d18072f03f4afc
Dhivi41 <dhiviraj2004@gmail.com>
2026-03-16 17:41:21 +0530

    Assignment24

---
+++ Java/33ServletWithJSP/src/main/java/com/servlet/LoginServlet.java
@@ -0,0 +1,31 @@
+package com.servlet;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+
+private static final long serialVersionUID = 1L;
+
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+
+String username = request.getParameter("username");
+String password = request.getParameter("password");
+
+if(username.equals("admin") && password.equals("1234"))
+{
+RequestDispatcher rd = request.getRequestDispatcher("dashboard.jsp");
+rd.forward(request,response);
+}
+else
+{
+request.setAttribute("error","Invalid Username or Password");
+
+RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+rd.forward(request,response);
+}
+
+}
+}
---
+++ Java/33ServletWithJSP/src/main/webapp/dashboard.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<!DOCTYPE html>
+<%@ page language="java" %>
+
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+
+<body>
+
+<h2>Login Successful</h2>
+
+Welcome to Dashboard
+
+</body>
+</html>
